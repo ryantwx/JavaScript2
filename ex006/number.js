2ed96bb88154131d5afc83d50a21af9b5c3c71ad
ryantwx <ryans2868@gmail.com>
2024-05-16 18:24:03 -0300

    Create number.js

---
+++ ex006/number.js
@@ -0,0 +1,6 @@
+let num1 = 1
+let num2 = 2.5213213123123123123
+console.log(num1.toString()+num2)//pois agora o num1 é considerado uma string
+//
+//
+console.log(num2.toFixed(2))//arredonda casas decimais

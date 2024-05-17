01b558ca1daa2ab884745fa0ab050793fe17f371
ryantwx <ryans2868@gmail.com>
2024-05-17 10:36:59 -0300

    Create math.js

---
+++ ex007-math/math.js
@@ -0,0 +1,14 @@
+// let num1 = 1.33123
+// let num2 = Math.round(num1)
+// if(num1>2){
+//     console.log(`numero acima do limite`)
+// }
+// else{
+//     console.log(`numero ${num1} aredondado para ${num2}`)
+// }
+
+// let gerar = Math.round(Math.random())
+// console.log(gerar)
+let num1 = 9
+console.log(num1** (1/2))//raiz quadrada
+let qwe= document.querySelector

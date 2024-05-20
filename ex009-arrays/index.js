bb97897f51d7f927f37f8157fa693c7f62914608
ryantwx <ryans2868@gmail.com>
2024-05-20 18:02:19 -0300

    sa

---
+++ ex009-arrays/index.js
@@ -0,0 +1,5 @@
+const alunos =[ 'ryan' ,'julia', 'maria']
+console.log(alunos.length)
+alunos.push("Ryan")
+console.log(alunos)
+console.log(alunos.length)
---
+++ ex010-função/index.js
@@ -0,0 +1,15 @@
+function saudação(nome){
+    return `ssadasad${nome}`
+    
+}
+const ola = saudação('luiz')
+console.log(ola)
+
+
+function some (x,y){
+    const soma = x + y
+    return soma
+}
+console.log(some(1,5))
+console.log(some(2,5))
+console.log(some(3,5))

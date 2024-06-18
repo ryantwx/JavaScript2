fc769be495441e23feb13022633fd2329e5c2845
ryantwx <ryans2868@gmail.com>
2024-06-18 10:50:36 -0300

    Update data.js

--- ex015/data.js
+++ ex015/data.js
@@ -1,4 +1,26 @@
-const dataAtual = new Date()
-const anoAtual = dataAtual.getHours();
+// const dataAtual = new Date()
+// const anoAtual = dataAtual.getHours();
 
-console.log(anoAtual)
+// console.log(anoAtual) 
+let  data = new Date()
+let dia = data.getDay()
+console.log(dia)
+
+switch(dia){
+    case 0:
+        dia ='domingo' 
+    break
+    case 1:
+        dia ='segunda' 
+    break
+    case 2:
+        dia='tera' 
+    break
+    case 3:
+        dia='quarta' 
+    break
+    case 4:
+        dia ='quinta' 
+    break
+}
+console.log(dia)

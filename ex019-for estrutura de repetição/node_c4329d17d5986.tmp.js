3dae237556252fbe74e65e8853a49e1c79befcb7
ryantwx <ryans2868@gmail.com>
2024-06-26 10:56:17 -0300

    Create node_c4329d17d5986.tmp.js

---
+++ ex019-for estrutura de repetição/node_c4329d17d5986.tmp.js
@@ -0,0 +1,17 @@
+// for(let i =0; i<=5 ;i++){
+//     console.log(i)
+// }
+
+// let frutas =['maça', 'pera' ,'uva']
+// for(let i in frutas){
+//     console.log(i ,frutas[i])
+// }
+
+const pessoa ={
+    nome:'ryan',
+    sobrenome: 'Silva',
+    idade: 20
+}
+for (let chave in pessoa){
+    console.log(pessoa[chave])
+}

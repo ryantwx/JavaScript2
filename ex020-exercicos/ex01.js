0191b321405524637d914e9309131e44624c1d42
ryantwx <ryans2868@gmail.com>
2024-07-01 15:47:27 -0300

    Create ex01.js

---
+++ ex020-exercicos/ex01.js
@@ -0,0 +1,10 @@
+//Escreve uma funcçao com dois numero e rotorne o maior deles
+function maior (x,y){
+    if(x>y){
+        return x
+    }
+    else{
+        return y
+    }
+}
+console.log(maior(5,0))

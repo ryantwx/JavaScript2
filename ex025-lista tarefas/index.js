3cbccbf693efa5c36501f991f498fce5216065e8
ryantwx <ryans2868@gmail.com>
2024-08-13 22:42:56 -0300

    lista tarefas

---
+++ ex025-lista tarefas/index.js
@@ -0,0 +1,39 @@
+
+
+function Armazenar(){
+
+    
+    var btnTarefa = document.getElementById('tarefa').value
+
+    if(btnTarefa===''){
+        window.alert('Não adicione tarefas vazias ')
+        return
+    }
+    else{
+        var listaMae = document.getElementById('listaMae')
+        var li = document.createElement('li')
+
+        var botao = document.createElement('button')
+        li.innerText= btnTarefa+ '      '
+        botao.innerText = 'Apagar'
+
+        
+
+        listaMae.appendChild(li)
+        li.appendChild(botao)
+
+        
+
+        botao.addEventListener('click', function(e){
+            listaMae.removeChild(li)
+
+    })
+
+    document.getElementById('tarefa').value=''
+    }
+
+    
+}
+
+
+

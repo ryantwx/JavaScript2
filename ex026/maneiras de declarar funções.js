95a8286b2c0850c11c0d64dddc5d7d9555152d9a
ryantwx <ryans2868@gmail.com>
2024-10-11 16:09:59 -0300

    jjj
    kk

---
+++ ex026/maneiras de declarar funções.js
@@ -0,0 +1,22 @@
+
+
+// pode ser chamdo em qualquer lugar do codigo 
+function Fala(){
+    console.log('oii')
+}
+
+//function classes 
+
+const SouUmDado = function(){
+    console.log('o2ii')
+}
+SouUmDado()
+//arrow function
+const nova = () => {
+    console.log('teste ')
+}
+nova()
+false
+
+
+

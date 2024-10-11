bc7ea82f4a04bd4037fa299da0ceb5edbcfdda5c
ryantwx <ryans2868@gmail.com>
2024-10-11 16:50:46 -0300

    teste 22
    dsf

--- ex026/maneiras de declarar funções.js
+++ ex026/maneiras de declarar funções.js
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-function
-=======
+
 
 
 // pode ser chamdo em qualquer lugar do codigo 
@@ -18,9 +16,4 @@ SouUmDado()
 const nova = () => {
     console.log('teste ')
 }
-nova()
-false
-
-
-
->>>>>>> 95a8286b2c0850c11c0d64dddc5d7d9555152d9a
+nova()//teste 2

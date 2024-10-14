4b6271edb557bd3479fe47709b26d4c772a822e3
ryantwx <ryans2868@gmail.com>
2024-10-14 10:29:07 -0300

    Create retorno de função .js

---
+++ ex026-funçoes/retorno de função .js
@@ -0,0 +1,28 @@
+// function criarPessoa(nome , sobrenome ){
+//     return { nome , sobrenome}
+// }
+// const p1 = criarPessoa( 'ryan' ,'Silva ')
+// console.log(p1)
+
+// function falaFrase(comeco){
+//     function falaResto(resto ){
+//         return comeco + '' + resto
+//     }
+//     return falaResto
+// }
+// const fala = falaFrase('ola')
+// const resto = fala( '  mundo ')
+// console.log(resto)
+
+function criaMultiplicador(multiplicador){
+    return function (n){
+        return n * multiplicador
+    }
+}
+const duplica = criaMultiplicador(2)
+const triplica = criaMultiplicador(3)
+const quadriplica = criaMultiplicador(4)
+
+console.log(duplica(2))
+console.log(triplica(2))
+console.log(quadriplica(2))
